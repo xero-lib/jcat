86036549c3c21830c04914895387ce3f574194d4
xero-lib <jonathankurtz@protonmail.com>
2023-10-16 14:47:57 -0500

    basic cat implementation

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		puts("Requires at least one file to read");
+		return 1;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		if (access(argv[i], F_OK) != 0) {
+			printf("Unable to find file: %s\n", argv[i]);
+			return 1;
+		}
+
+		if (access(argv[i], R_OK) != 0) {
+			printf("Unable to read file: %s\n", argv[i]);
+			return 1;
+		}
+
+
+	 	FILE *fptr = fopen(argv[i], "r");
+		char string[500];
+	
+		while (fgets(string, 500, fptr)) {
+			printf("%s", string);
+		}
+		
+		puts("\n");
+	}
+
+	return 0;
+}
